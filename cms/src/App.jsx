578b34946861cce87067a1e1d58ace6fe4bd34b3
vincentiusevan <vincent.evan05@gmail.com>
2026-03-31 17:52:50 +0700

    crud admin

--- cms/src/App.jsx
+++ cms/src/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router";
-import AuthLayout from "./layouts/AuthLayout";
-import MainLayout from "./layouts/MainLayout";
-import Register from "./views/auth/Register";
-import Login from "./views/auth/Login";
-import RegisterDoctor from "./views/admin/RegisterDoctor";
-import Home from "./views/user/Home";
-import BookingPage from "./views/user/BookingPage";
-import Chat from "./views/user/Chat";
-import Chats from "./views/user/Chats";
+import { Routes, Route, Navigate } from 'react-router';
+import AuthLayout from './layouts/AuthLayout';
+import MainLayout from './layouts/MainLayout';
+import Register from './views/auth/Register';
+import Login from './views/auth/Login';
+import RegisterDoctor from './views/admin/RegisterDoctor';
+import Home from './views/user/Home';
+import BookingPage from './views/user/BookingPage';
+import ManageDoctors from './views/admin/ManageDoctors';
+import ManageUsers from './views/admin/ManageUsers';
+// import Chat from './views/user/Chat';
+// import Chats from './views/user/Chats';
 
 function App() {
   return (
@@ -19,10 +21,13 @@ function App() {
         </Route>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/doctor-register" element={<RegisterDoctor />}></Route>
-          <Route path="/chat" element={<Chats />} />
+          <Route path="/doctor-register" element={<RegisterDoctor />} />
+          {/* <Route path="/chat" element={<Chats />} /> */}
           <Route path="/booking/:doctorId" element={<BookingPage />} />
-          <Route path="/chat/:chatRoomId" element={<Chat />} />
+          {/* <Route path="/chat/:chatRoomId" element={<Chat />} /> */}
+          <Route path="/admin" element={<Navigate to="/admin/doctors" />}></Route>
+          <Route path="/admin/doctors" element={<ManageDoctors />} />
+          <Route path="/admin/users" element={<ManageUsers />} />
         </Route>
       </Routes>
     </>
